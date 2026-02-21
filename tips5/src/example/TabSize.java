427ea1230afe5b90a3726f8ef5f6336a53b5ea24
Watanabe Iwao <Iwao.Watanabe@jp.ey.com>
2026-02-21 11:10:40 +0900

    tips5 取り込み

--- tips5/src/example/TabSize.java
+++ tips5/src/example/TabSize.java
@@ -1,4 +1,4 @@
-
+package example;
 //-*- mode:java; encoding:utf8n; coding:utf-8 -*-
 // vim:set fileencoding=utf-8:
 //@homepage@
--- tips5/src/example/TabThumbnail.java
+++ tips5/src/example/TabThumbnail.java
@@ -13,7 +13,7 @@ class TabThumbnail extends JPanel {
     private final MyTabbedPane tab = new MyTabbedPane();
     public TabThumbnail() {
         super(new BorderLayout());
-        ToolTipManager.sharedInstance().setDismissDelay(Integer.MAX_VALUE);
+        javax.swing.ToolTipManager.sharedInstance().setDismissDelay(Integer.MAX_VALUE);
         ImageIcon icon = new ImageIcon(getClass().getResource("wi0124-48.png"));
         tab.addTab("wi0124-48.png", null, new JLabel(icon), "dummy");
         //addImageTab(tab, getClass().getResource("wi0124-48.png"));
